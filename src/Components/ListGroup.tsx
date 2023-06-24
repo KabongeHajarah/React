9427a93d82f1b9df03709580d7fd301d1c4d36a1
KabongeHajarah <kabongehajarah@gmail.com>
2023-06-24 17:01:02 +0400

    Adding Components

---
+++ src/Components/ListGroup.tsx
@@ -0,0 +1,12 @@
+function ListGroup() {
+  return (
+    <ul className="list-group">
+      <li className="list-group-item">Names</li>
+      <li className="list-group-item">Bellah</li>
+      <li className="list-group-item">Maleek</li>
+      <li className="list-group-item">Rayan</li>
+      <li className="list-group-item">Shaturah</li>
+    </ul>
+  );
+}
+export default ListGroup;
