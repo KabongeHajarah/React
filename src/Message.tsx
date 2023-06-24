efe02027b6fa2acd2926c6eb33c966f4a7673ab1
KabongeHajarah <kabongehajarah@gmail.com>
2023-06-24 16:40:50 +0400

    Installing Bootstrap

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,6 +1,9 @@
 //PascalCasing
 function Message() {
     //jsx: Javascript xml
+    const name='Bellah'
+    if (name)
+    return <h1>Hello {name}</h1>;
     return <h1>Hello World</h1>;
 }
 export default Message;
