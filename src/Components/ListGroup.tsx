1ea53f2697855aa53ca2da97c2b580596a95eb5b
KabongeHajarah <kabongehajarah@gmail.com>
2023-06-24 17:25:52 +0400

    Wrapping multiple elements in a fragment

--- src/Components/ListGroup.tsx
+++ src/Components/ListGroup.tsx
@@ -1,12 +1,16 @@
+import { Fragment } from "react";
 function ListGroup() {
   return (
-    <ul className="list-group">
-      <li className="list-group-item">Names</li>
-      <li className="list-group-item">Bellah</li>
-      <li className="list-group-item">Maleek</li>
-      <li className="list-group-item">Rayan</li>
-      <li className="list-group-item">Shaturah</li>
-    </ul>
+    <Fragment>
+         <h1>List</h1>
+        <ul className="list-group">
+          <li className="list-group-item">Names</li>
+          <li className="list-group-item">Bellah</li>
+          <li className="list-group-item">Maleek</li>
+          <li className="list-group-item">Rayan</li>
+          <li className="list-group-item">Shaturah</li>
+        </ul>
+    </Fragment>
   );
 }
 export default ListGroup;
